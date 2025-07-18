803ba0067ebcfb4d9bf3ead53e62039038f8d987
NolanEmery <nolanemery1206@gmail.com>
2025-07-18 11:56:08 -0700

    eighth version of the app

---
+++ src/app/questions.js
@@ -0,0 +1,150 @@
+export const numberQuestionArray = [
+  {
+    question: "What is 7+5?",
+    answer: 12,
+  },
+  {
+    question: "What is 14/2?",
+    answer: 7,
+  },
+  {
+    question: "What is 12*5?",
+    answer: 60,
+  },
+  {
+    question: "What is 198-36?",
+    answer: 162,
+  },
+  {
+    question: "What is 121/11?",
+    answer: 11,
+  },
+  {
+    question: "What is 12*12?",
+    answer: 144,
+  },
+  {
+    question: "What is 76+80?",
+    answer: 156,
+  },
+  {
+    question: "What is 15-6?",
+    answer: 9,
+  },
+  {
+    question: "What is 8+8?",
+    answer: 16,
+  },
+  {
+    question: "What is 19-2?",
+    answer: 17,
+  },
+  {
+    question: "What is 4*6?",
+    answer: 24,
+  },
+  {
+    question: "What is 72/8?",
+    answer: 9,
+  },
+];
+export const booleanQuestionArray = [
+  {
+    question: "T/F: A rectangle has three sides.",
+    answer: "false",
+  },
+  {
+    question: "T/F: The first six digits of pi are 3.14159.",
+    answer: "true",
+  },
+  {
+    question: "T/F: The product of two even numbers is even.",
+    answer: "true",
+  },
+  {
+    question: "T/F: The product of two odd numbers is odd.",
+    answer: "true",
+  },
+  {
+    question: "T/F: The sum of two even numbers is even.",
+    answer: "true",
+  },
+  {
+    question: "T/F: The sides of a triangle are always the same length.",
+    answer: "false",
+  },
+  {
+    question: "T/F: All angles in a triangle are always the same.",
+    answer: "false",
+  },
+  {
+    question: "T/F: All sides in a rectangle are always the same length.",
+    answer: "false",
+  },
+  {
+    question: "T/F: A triangle has four sides.",
+    answer: "false",
+  },
+  {
+    question: "T/F: A rhombus has four sides.",
+    answer: "true",
+  },
+  {
+    question: "T/F: A trapezoid has five sides.",
+    answer: "false",
+  },
+  {
+    question: "T/F: A rectangular prism has six surfaces.",
+    answer: "true",
+  },
+];
+export const stringQuestionArray = [
+  {
+    question: "What is the capital of Canada?",
+    answer: "Ottawa",
+  },
+  {
+    question: "Which ocean borders the west coast of Canada?",
+    answer: "Pacific",
+  },
+  {
+    question: "What is the name of the North Star?",
+    answer: "Polaris",
+  },
+  {
+    question: "What is the capital of the U.S.A.?",
+    answer: "Washington, D.C.",
+  },
+  {
+    question: "In which constellation lies the Big Dipper?",
+    answer: "Ursa Major",
+  },
+  {
+    question: "What is the shape of Earth?",
+    answer: "Sphere",
+  },
+  {
+    question: "What is the lowest layer of the atmosphere?",
+    answer: "Troposphere",
+  },
+  {
+    question: "What is the most abundant element in the air?",
+    answer: "Nitrogen",
+  },
+  {
+    question: "What is the southernmost continent?",
+    answer: "Antarctica",
+  },
+  {
+    question: "What is the capital of Russia?",
+    answer: "Moscow",
+  },
+  {
+    question: "Which particle orbits the nucleus of the atom?",
+    answer: "Electron",
+  },
+  {
+    question: "What is the brightest star in the sky?",
+    answer: "Sirius",
+  },
+];
